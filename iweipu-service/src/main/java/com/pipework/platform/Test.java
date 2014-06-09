0081b4949b3c0b05df682ca81585e83844956ea0
lixiang <395569664@qq.com>
2014-06-09 18:26:37 +0800

    add nexus cfg

---
+++ iweipu-service/src/main/java/com/pipework/platform/Test.java
@@ -0,0 +1,10 @@
+package com.pipework.platform;
+
+/**
+ * Description:
+ * User: lixiang
+ * Date: 14-6-9
+ * Time: 上午10:37
+ */
+public class Test {
+}
---
+++ iweipu-service/src/main/java/com/pipework/platform/api/impl/AccountServiceImpl.java
@@ -0,0 +1,12 @@
+package com.pipework.platform.api.impl;
+
+import com.pipework.api.account.IAccountService;
+
+/**
+ * Description:
+ * User: lixiang
+ * Date: 14-6-9
+ * Time: 上午10:57
+ */
+public class AccountServiceImpl implements IAccountService {
+}
